514743d741e9473b4d1d8272620f6f28db56db41
Josh-kean01 <115477703+Josh-kean01@users.noreply.github.com>
2022-12-21 12:41:38 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,26 +3,26 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="fixed-bottom bottom-0 yuuyi mt-3">
-//       <div className="d-flex justify-content-center">
-//         <a
-//           className="px-3"
-//           href="https://www.facebook.com/joshua.adekunle.31542"
-//         >
-//           <iconify-icon icon="ic:outline-facebook" width="50"></iconify-icon>
-//         </a>
-//         <a className="px-3" href="https://www.instagram.com/">
-//           <iconify-icon icon="mdi:instagram" width="50"></iconify-icon>
-//         </a>
-//         <a className="px-3" href="https://github.com/">
-//           <iconify-icon icon="mdi:github" width="50"></iconify-icon>
-//         </a>
-//         <a className="px-3" href="https://wa.me/+2349157284517">
-//           <iconify-icon icon="logos:whatsapp-icon" width="50"></iconify-icon>
-//         </a>
-//         <a className="px-3" href="https://twitter.com/">
-//           <iconify-icon icon="mdi:twitter" width="50"></iconify-icon>
-//         </a>
-//       </div>
+      <div className="d-flex justify-content-center">
+        <a
+          className="px-3"
+          href="https://www.facebook.com/joshua.adekunle.31542"
+        >
+          <iconify-icon icon="ic:outline-facebook" width="50"></iconify-icon>
+        </a>
+        <a className="px-3" href="https://www.instagram.com/">
+          <iconify-icon icon="mdi:instagram" width="50"></iconify-icon>
+        </a>
+        <a className="px-3" href="https://github.com/">
+          <iconify-icon icon="mdi:github" width="50"></iconify-icon>
+        </a>
+        <a className="px-3" href="https://wa.me/+2349157284517">
+          <iconify-icon icon="logos:whatsapp-icon" width="50"></iconify-icon>
+        </a>
+        <a className="px-3" href="https://twitter.com/">
+          <iconify-icon icon="mdi:twitter" width="50"></iconify-icon>
+        </a>
+      </div>
     </footer>
   );
 };
