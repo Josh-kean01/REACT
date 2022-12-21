32a7ba66aca9544a4bbbaad0dab2c03db659254b
Josh-kean01 <115477703+Josh-kean01@users.noreply.github.com>
2022-12-21 12:16:19 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,24 +2,24 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="sticky-bottom">
-      <div className="d-flex justify-content-center pt-3">
+    <footer className="fixed-bottom bottom-0 yuuyi">
+      <div className="d-flex justify-content-center">
         <a
-          className="px-4"
+          className="px-3"
           href="https://www.facebook.com/joshua.adekunle.31542"
         >
           <iconify-icon icon="ic:outline-facebook" width="50"></iconify-icon>
         </a>
-        <a className="px-4" href="https://www.instagram.com/">
+        <a className="px-3" href="https://www.instagram.com/">
           <iconify-icon icon="mdi:instagram" width="50"></iconify-icon>
         </a>
-        <a className="px-4" href="https://github.com/">
+        <a className="px-3" href="https://github.com/">
           <iconify-icon icon="mdi:github" width="50"></iconify-icon>
         </a>
-        <a className="px-4" href="https://wa.me/+2349157284517">
+        <a className="px-3" href="https://wa.me/+2349157284517">
           <iconify-icon icon="logos:whatsapp-icon" width="50"></iconify-icon>
         </a>
-        <a className="px-4" href="https://twitter.com/">
+        <a className="px-3" href="https://twitter.com/">
           <iconify-icon icon="mdi:twitter" width="50"></iconify-icon>
         </a>
       </div>
